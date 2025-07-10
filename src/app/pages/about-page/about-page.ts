3869e955c69a6f7440ef688e0399df8fcb36a47e
marco-soria <marco.soriaa7@gmail.com>
2025-07-10 09:47:25 -0500

    edited pages

--- src/app/pages/about-page/about-page.ts
+++ src/app/pages/about-page/about-page.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about-page',
   imports: [],
   templateUrl: './about-page.html',
 })
-export default class AboutPage {}
+export default class AboutPage implements OnInit {
+  private title = inject(Title);
+  private meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.title.setTitle('About Page');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Este es mi About Page',
+    });
+    this.meta.updateTag({ name: 'og:title', content: 'About Page' });
+    this.meta.updateTag({
+      name: 'keywords',
+      content: 'Hola,Mundo,Curso,Angular,PRO',
+    });
+  }
+}
--- src/app/pages/pricing-page/pricing-page.ts
+++ src/app/pages/pricing-page/pricing-page.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pricing-page',
   imports: [],
   templateUrl: './pricing-page.html',
 })
-export default class PricingPage {}
+export default class PricingPage implements OnInit {
+  private title = inject(Title);
+  private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
+
+  ngOnInit(): void {
+    // if (isPlatformBrowser(this.platform)) {
+    //   document.title = 'Pricing Page';
+    // }
+
+    // console.log({ hola: 'mundo' });
+
+    this.title.setTitle('Pricing Page');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Este es mi Pricing Page',
+    });
+    this.meta.updateTag({ name: 'og:title', content: 'Pricing Page' });
+  }
+}
