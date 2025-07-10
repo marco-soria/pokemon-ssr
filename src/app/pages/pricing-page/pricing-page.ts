7e3da03f0d98fa444b7e99d8050763dd2fc1a8a0
marco-soria <marco.soriaa7@gmail.com>
2025-07-10 08:34:39 -0500

    added main pages and routing

---
+++ src/app/pages/pricing-page/pricing-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pricing-page',
+  imports: [],
+  templateUrl: './pricing-page.html',
+})
+export default class PricingPage {}
